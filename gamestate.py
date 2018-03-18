303541a5a6de2b53c9d8a0bd5d92af07771d130c
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-03-18 09:09:49 -0700

    init gamestate

---
+++ gamestate.py
@@ -0,0 +1,13 @@
+
+class GameState:
+
+    def __init__(self):
+        # keep track of which cells are opened/closed
+        # identify which player has initiative
+        # record the current location of each player
+        print("init")
+
+
+if __name__ == "__main__":
+    print("starting...")
+    emptyState = GameState()  # create an instance of the object
